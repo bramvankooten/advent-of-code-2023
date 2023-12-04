a9864f6e2f228c2a8152dee6619c13315b3941b9
Bram van Kooten <bram.vankooten@hotmail.com>
2023-12-05 00:27:07 +0100

    Day 3 complete

--- day3.py
+++ day3.py
@@ -8,6 +8,7 @@ file = open('data/day3.in', 'r')
 lines = file.readlines()
 digits = []
 symbols = []
+stars = []
 
 def digit_near_symbol(start, end, digit_range):
     for i in range(start,end+1):
@@ -16,12 +17,29 @@ def digit_near_symbol(start, end, digit_range):
             return True
     return False
 
+def find_gears(start, end, star):
+    gears = []
+    for i in range(start,end+1):
+        for d in digits[i]:
+            digit_range = list(range(d[0]-1,d[1]+1))
+            if star in digit_range:
+                gears.append(int(lines[i][d[0]:d[1]]))
+
+    if len(gears) == 2:
+        return gears[0] * gears[1]
+    else:
+        return 0
+
+
 for line in lines:
     line = line.strip()
     digit = [(m.start(0), m.end(0)) for m in re.finditer('\d+', line)]
     digits.append(digit)
     symbol = [m.start(0) for m in re.finditer('[^\w.]', line)]
     symbols.append(symbol)
+    star = [m.start(0) for m in re.finditer('\*', line)]
+    stars.append(star)
+
 
 total_part_one = 0
 
@@ -39,4 +57,18 @@ for i, line in enumerate(lines):
         if near:
             total_part_one += int(line[d[0]:d[1]])
 
-print('Part one:',total_part_one)
+print('Part one:', total_part_one)
+
+total_part_two = 0
+
+for i, line in enumerate(lines):
+    for s in stars[i]:
+        if i == 0:
+            gear_value = find_gears(i,i+1, s)
+        elif i == len(lines) - 1:
+            gear_value = find_gears(i-1,i, s)
+        else:
+            gear_value = find_gears(i-1, i+1, s)
+        total_part_two += gear_value
+
+print('Part two:', total_part_two)
