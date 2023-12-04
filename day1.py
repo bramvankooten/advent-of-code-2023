dd65ce693b357b81900fe7fa3cf2fa1f2ded0b1e
Bram van Kooten <bram.vankooten@hotmail.com>
2023-12-04 18:44:48 +0100

    Complete day 1

--- day1.py
+++ day1.py
@@ -1,14 +1,37 @@
 import re
 
+# file = open('data/day1_2.test', 'r')
 file = open('data/day1_1.in', 'r')
 
-lines = [l.strip() for l in file.readlines()]
+lines = file.readlines()
 
+def convert_to_num(string):
+    if string == 'one':
+        return '1'
+    elif string == 'two':
+        return '2'
+    elif string == 'three':
+        return '3'
+    elif string == 'four':
+        return '4'
+    elif string == 'five':
+        return '5'
+    elif string == 'six':
+        return '6'
+    elif string == 'seven':
+        return '7'
+    elif string == 'eight':
+        return '8'
+    elif string == 'nine':
+        return '9'
+    else:
+        return string
+    
 total = 0
 
 for line in lines:
-    digits = [d for d in re.findall(r'\d', line)]
-    num = int(digits[0] + digits[-1])
+    digits = [d for d in re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)]
+    num = int(convert_to_num(digits[0]) + convert_to_num(digits[-1]))
     total += num
     
 print(total)
